37af873534c09a3c4ce2bca1d8aa4202ead72c21
Joseph Berry <josephberry910@gmail.com>
2021-09-08 15:25:27 -0600

    problems 1-9 completed

--- lab.js
+++ lab.js
@@ -150,7 +150,6 @@ if(changeMyMind){
 };
 
 
-
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
